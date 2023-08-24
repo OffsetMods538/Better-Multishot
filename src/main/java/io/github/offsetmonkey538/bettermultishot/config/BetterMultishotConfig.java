debae717201a43be0a7d1c3a39d9fbad6b76efce
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-08-24 13:35:08 +0200

    Update config library

--- src/main/java/io/github/offsetmonkey538/bettermultishot/config/BetterMultishotConfig.java
+++ src/main/java/io/github/offsetmonkey538/bettermultishot/config/BetterMultishotConfig.java
@@ -1,16 +1,9 @@
 package io.github.offsetmonkey538.bettermultishot.config;
 
+import blue.endless.jankson.Comment;
 import top.offsetmonkey538.monkeyconfig538.Config;
-import top.offsetmonkey538.monkeyconfig538.annotation.ConfigEntry;
-
-import static io.github.offsetmonkey538.bettermultishot.entrypoint.BetterMultishotMain.*;
 
 public class BetterMultishotConfig extends Config {
-    @ConfigEntry("Set to true if you want multishot arrows from bows to do half the damage. Set to false if now. Default is true.")
-    public static boolean nerfBowMultishot = true;
-
-    @Override
-    protected String getName() {
-        return MOD_ID;
-    }
+    @Comment("Set to true if you want multishot arrows from bows to do half the damage. Set to false if now. Default is true.")
+    public boolean nerfBowMultishot = true;
 }
--- src/main/java/io/github/offsetmonkey538/bettermultishot/entrypoint/BetterMultishotMain.java
+++ src/main/java/io/github/offsetmonkey538/bettermultishot/entrypoint/BetterMultishotMain.java
@@ -4,6 +4,7 @@ import io.github.offsetmonkey538.bettermultishot.config.BetterMultishotConfig;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import top.offsetmonkey538.monkeyconfig538.ConfigManager;
 
 public class BetterMultishotMain implements ModInitializer {
 	public static final String MOD_ID = "bettermultishot";
@@ -11,6 +12,10 @@ public class BetterMultishotMain implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		new BetterMultishotConfig().init();
+		ConfigManager.init(new BetterMultishotConfig(), MOD_ID);
+	}
+
+	public static BetterMultishotConfig config() {
+		return (BetterMultishotConfig) ConfigManager.get(MOD_ID);
 	}
 }
