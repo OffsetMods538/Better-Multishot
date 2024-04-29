11277347b698b00bf47e1084c97b902143955664
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-04-29 22:04:06 +0300

    Service loader

--- common/src/main/java/top/offsetmonkey538/bettermultishot/BetterMultishot.java
+++ common/src/main/java/top/offsetmonkey538/bettermultishot/BetterMultishot.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ServiceLoader;
+
 public class BetterMultishot implements ModInitializer {
 	public static final String MOD_ID = "bettermultishot";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -12,5 +14,13 @@ public class BetterMultishot implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+	}
+
+	public static <T> T load(Class<T> clazz) {
+		final T loaded = ServiceLoader.load(clazz)
+				.findFirst()
+				.orElseThrow(() -> new RuntimeException("Failed to load service for " + clazz.getName()));
+		LOGGER.debug("Loaded '{}' for service '{}'!", loaded, clazz);
+		return loaded;
 	}
 }
