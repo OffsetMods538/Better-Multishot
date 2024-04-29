625df90acbfdaa0eda0cde9f96729bae0f228621
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-04-29 19:31:38 +0300

    Set up multi-version project

---
+++ common/src/main/java/top/offsetmonkey538/bettermultishot/BetterMultishot.java
@@ -0,0 +1,16 @@
+package top.offsetmonkey538.bettermultishot;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterMultishot implements ModInitializer {
+	public static final String MOD_ID = "bettermultishot";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
